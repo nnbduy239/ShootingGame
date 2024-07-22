dcc6278191099fc507a5df86be3ea5d4b882f1dd
nnbduy239 <nnbduy239@gmail.com>
2024-07-23 01:56:06 +0700

    replace animations and add gun node to player character

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
+@export var speed = 400
 
 func _process(delta):
-	if velocity.x > 0.0:
-		$AnimationPlayer.play("run_right")
-	elif velocity.x < 0.0:
-		$AnimationPlayer.play("run_left")
-	elif velocity.y > 0.0:
-		$AnimationPlayer.play("run_down")
-	elif velocity.y < 0.0:
-		$AnimationPlayer.play("run_up")
-	else:
-		$AnimationPlayer.play("idle")
+	update_animation()
+	
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 400
+	velocity = direction * speed
 	move_and_slide()
+func update_animation():
+	if velocity.length() > 0.0:
+		$AnimationPlayer.play("run")
+		$Sprite2D.flip_h = velocity.x < 0
+		$Gun/WeaponPivot/Pistol.flip_h = velocity.x < 0
+	else:
+		$AnimationPlayer.play("idle")
+
