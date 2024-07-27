da2fa6e9024cacdefffe070b37cd41d2107527ca
nnbduy239 <nnbduy239@gmail.com>
2024-07-27 15:14:27 +0700

    add enemy health bar

--- Scripts/Boss/boss.gd
+++ Scripts/Boss/boss.gd
@@ -9,15 +9,19 @@ extends CharacterBody2D
 @onready var collision = $CollisionShape2D
 @onready var timer = $Timer
 @onready var state_machine = $StateMachine
+@onready var health_bar = $HealthBar
 
 func _ready():
 	stats.health = stats.max_health
+	health_bar.init_health(stats.health)
+
 
 func _physics_process(delta):
 	move_and_slide()
 func _on_hurt_box_area_entered(area):
 	if not area.is_in_group("enemies"):
 		hit_animaiton.play("RESET")
+		health_bar.health = stats.health
 		if stats.health <= 0:
 			state_machine.on_child_transition(state_machine.current_state, "Death")
 func _on_timer_timeout():
